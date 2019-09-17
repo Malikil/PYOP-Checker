3ae06fb7e77ed420d9c2327d1c093422e89cf3b3
Malikil <eboy.coltt5@gmail.com>
2019-09-17 10:23:20 -0700

    Finish map checker
    Everything in the checker module should be self-contained now. I should probably move the logic for connecting to the osu api itself into a different module, but for now this should work properly.
    
    No logging yet, that should get added if I ever remember to.

--- checker.js
+++ checker.js
@@ -84,182 +84,109 @@ function checkPool(maps)
 }
 
 /**
- * @param {*} map An object with the map's id and mod
- * @param {String} user The username of the player
- * @returns {Promise<any>} Returns a promise which will resolve to an object
- *     containing the pass/fail status and the full map info
+ * Checks a single beatmap for simple itmes like drain time, star rating, and mode
+ * @param {*} beatmap The beatmap to check
+ * @returns The beatmap object, along with pass/fail status. If the map fails
+ *     a fail message will also be included.
+ *     {
+ *         beatmap: [Object],
+ *         passed: false,
+ *         message: "Drain time is too long"
+ *     }
  */
-function checkMap(map, range, user)
+function quickCheck(beatmap, userid)
 {
-    return new Promise((resolve, reject) => {
-        if (Number.isInteger(map.id) && map.id > 0)
-        {
-            var min, max;
-            if (range == "15k")
+    // Prepare response object
+    let result = {
+        beatmap: beatmap,
+        passed: false
+    };
+    // Check the game mode
+    if (beatmap.mode != 0)
+        result.message = "This map is for the wrong gamemode";
+    // Check drain time
+    else if (beatmap.hit_length - drainBuffer > maxLength)
+        result.message = `Drain time is above the ${convertSeconds(maxLength)} maximum. (${convertSeconds(beatmap.hit_length)})`;
+    else if (beatmap.hit_length + drainBuffer < minLength)
+        result.message = `Drain time is below the ${convertSeconds(maxLength)} minimum. (${convertSeconds(beatmap.hit_length)})`;
+    // Check total time
+    else if (beatmap.total_length > absoluteMax)
+        result.message = `Total map time is above the ${convertSeconds(absoluteMax)} limit. (${convertSeconds(beatmap.total_length)})`;
+    // Check difficulty
+    else if (beatmap.difficultyrating > maxStar)
+        result.message = `Star rating is above the ${maxStar} maximum. (${beatmap.difficultyrating})`;
+    else if (beatmap.difficultyrating < minStar)
+        result.message = `Star rating is below the ${minStar} minimum. (${beatmap.difficultyrating})`;
+    // Make sure the user didn't make this map themself
+    else if (beatmap.approved != 1 && beatmap.creator_id == userid)
+        result.message = `You can't submit your own maps unless they're ranked`;
+    else
+        result.passed = true;
+    return result;
+}
+
+/**
+ * Checks a given map for leaderboard info.
+ * Will get leaderboard from server, and check the number of scores, and
+ * whether the user has a score.
+ * @param {Number} mapid The map id to get leaderboard info for
+ * @param {Number} mod Bitwise representation of mods to check for
+ * @param {Number} userid The user to check on the leaderboard for
+ * @returns {Promise<boolean>} Whether the leaderboard would make the map accepted
+ */
+function leaderboardCheck(mapid, mod, userid)
+{
+    return fetch(`${osuapi}/get_scores?k=${key}&b=${mapid}&mods=${mod & DIFFMODS}`)
+        .then(response => response.json())
+        .then(scores => {
+            // The leaderboard passes if there are more than 'n' scores, if the
+            // first score is perfect, or if the user themself has a score
+            if (scores.length >= leaderboard
+                    || scores[0].perfect == 1
+                    || scores.find(score => score.user_id == userid) !== undefined)
+                return true;
+            return false;
+        });
+}
+
+/**
+ * Gets a single beatmap from the server, and verifies all values are proper
+ * @param {Number} mapid The map id to get info for
+ * @param {Number} mod The bitwise value of the selected mods
+ */
+function getBeatmap(mapid, mod)
+{
+    return fetch(`${osuapi}/get_beatmaps?k=${key}&b=${mapid}&mods=${mod & DIFFMODS}`)
+        .then(response => response.json())
+        .then(data => data[0])
+        .then(beatmap => {
+            if (!beatmap)
+                return undefined;
+            // Parse ints/floats
+            beatmap.hit_length = parseInt(beatmap.hit_length);
+            beatmap.total_length = parseInt(beatmap.total_length);
+            // Update length if DT/HT
+            if (mod & DT)
             {
-                min = fiftMin;
-                max = fiftMax;
+                beatmap.hit_length = (beatmap.hit_length * (2.0 / 3.0)) | 0;
+                beatmap.total_length = (beatmap.total_length * (2.0 / 3.0)) | 0;
             }
-            else
+            else if (mod & HT)
             {
-                min = minStar;
-                max = maxStar;
+                beatmap.hit_length = (beatmap.hit_length * (4.0 / 3.0)) | 0;
+                beatmap.total_length = (beatmap.total_length * (4.0 / 3.0)) | 0;
             }
-            fetch(`${osuapi}/get_beatmaps?k=${key}&b=${map.id}&mods=${map.mod & DIFFMODS}`)
-            .then(response => response.json())
-            .then(data => data[0])
-            .then(/** @param {Beatmap} beatmap */ beatmap => {
-                // Make sure the response is legible
-                if (!!!beatmap)
-                    return ({
-                        passed: undefined
-                    });
-                console.log(`Checking map ${beatmap.beatmap_id}`);
-                // Make sure it's a standard map
-                if (beatmap.mode != 0)
-                    return ({
-                        passed: false,
-                        reject: {
-                            map: map,
-                            reason: "This isn't a standard map. Did you post the wrong link/id?"
-                        }
-                    });
-                // Prepare all the variables used to check
-                let stars = parseFloat(parseFloat(beatmap.difficultyrating).toFixed(2));
-                console.log(`Stars: ${stars} (${beatmap.difficultyrating})`);
-                // Drain time
-                let length = parseInt(beatmap.hit_length);
-                let fullLength = parseInt(beatmap.total_length);
-                // Take DT/HT into account
-                if (map.mod & DT)
-                {
-                    length *= (2.0 / 3.0);
-                    fullLength *= (2.0 / 3.0);
-                    length = length | 0;
-                    fullLength = fullLength | 0;
-                }
-                else if (map.mod & HT)
-                {
-                    length *= (4.0 / 3.0);
-                    fullLength *= (4.0 / 3.0);
-                    length = length | 0;
-                    fullLength = fullLength | 0;
-                }
-                console.log(`Drain after mods: ${length} (mods ${map.mod})`);
-                // Last updated
-                let updated = new Date(beatmap.last_update);
-                console.log(`Date string: ${beatmap.last_update} => ${updated}`);
-                console.log(`Ranked status: ${beatmap.approved} (Ranked => 1)`);
-                // Check values
-                // Total length
-                if (fullLength > absoluteMax)
-                    return ({
-                        passed: false,
-                        reject: {
-                            map: map,
-                            reason: `The total length of this map is over the ${convertSeconds(absoluteMax)} maximum limit. (${convertSeconds(fullLength)})`
-                        },
-                        drain: length
-                    });
-                // Check the drain time
-                else if (length < minLength - drainBuffer)
-                {
-                    console.log(`${length} < ${minLength - drainBuffer} returned true`);
-                    return ({
-                        passed: false,
-                        reject: {
-                            map: map,
-                            reason: `The drain time of this map is more than ${drainBuffer} seconds under the ${convertSeconds(minLength)} minimum. (${convertSeconds(length)})`
-                        },
-                        drain: length
-                    });
-                }
-                else if (length > maxLength + drainBuffer)
-                {
-                    console.log(`${length} > ${maxLength + drainBuffer} returned true`);
-                    return ({
-                        passed: false,
-                        reject: {
-                            map: map,
-                            reason: `The drain time of this map is more than ${drainBuffer} seconds over the ${convertSeconds(maxLength)} maximum. (${convertSeconds(length)})`
-                        },
-                        drain: length
-                    });
-                }
-                // Check stars
-                else if (stars < min)
-                    return ({
-                        passed: false,
-                        reject: {
-                            map: map,
-                            reason: `The star rating of this map is below the ${min} minimum allowed for this week. (${stars})`
-                        },
-                        drain: length
-                    });
-                else if (stars > max)
-                    return ({
-                        passed: false,
-                        reject: {
-                            map: map,
-                            reason: `The star rating of this map is above the ${max} maximum allowed for this week. (${stars})`
-                        },
-                        drain: length
-                    });
-                // Check date
-                else if (updated < earliest)
-                    return ({
-                        passed: false,
-                        reject: {
-                            map: map,
-                            reason: `This map is from ${updated.getFullYear()}, maps before ${earliest.getFullYear()} aren't allowed for this time.`
-                        },
-                        drain: length
-                    });
-                // If the map is ranked proper, it can probably get partially approved. Otherwise it should be left to manual
-                else if (beatmap.approved == 1)
-                    return ({
-                        passed: true,
-                        map: map,
-                        drain: length
-                    });
-                else
-                    return ({
-                        passed: undefined,
-                        map: map,
-                        drain: length
-                    });
-            }).then(status => {
-                if (status.passed)
-                    fetch(`${osuapi}/get_scores?k=${key}&b=${map.id}&mods=${map.mod}`)
-                    .then(response => response.json())
-                    .then(/** @param {Score[]} scores */scores => {
-                        console.log(`Found ${scores.length} scores`);
-                        if (scores.length > leaderboard)
-                            resolve(status);
-                        else if (scores[0].perfect == 1)
-                            resolve(status);
-                        else
-                        {
-                            let passed = false;
-                            // The top play isn't an fc, and there are less than 20 plays
-                            // Check if the user has one of the plays
-                            for (let i = 0; i < scores.length && !passed; i++)
-                                if (scores[i].username == user)
-                                    passed = true;
-                            if (!passed)
-                                status.passed = undefined;
-                        }
-                    });
-                else
-                    resolve(status);
-            })
-        }
-        else
-            resolve({ passed: undefined });
-    });
+            beatmap.difficultyrating = parseFloat(parseFloat(beatmap.difficultyrating).toFixed(2));
+            beatmap.mode = parseInt(beatmap.mode);
+            beatmap.approved = parseInt(beatmap.approved);
+            beatmap.last_update = new Date(beatmap.last_update);
+            return beatmap;
+        })
 }
 
 module.exports = {
-    checkMap,
-    checkPool
+    quickCheck,
+    leaderboardCheck,
+    checkPool,
+    getBeatmap
 }
