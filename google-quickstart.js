78c35e2d9130c07d37f11e56deb8cbc9aa879171
Malikil <eboy.coltt5@gmail.com>
2019-10-01 17:39:21 -0700

    Load authentication from environment vars

--- google-quickstart.js
+++ google-quickstart.js
@@ -1,28 +1,29 @@
 const {google} = require('googleapis');
 const util = require('util');
 const sheets = google.sheets('v4');
-process.env.GOOGLE_APPLICATION_CREDENTIALS = './gsheet-credentials.json';
 
-const auth = new google.auth.GoogleAuth({
-    scopes: ['https://www.googleapis.com/auth/spreadsheets']
-});
-
-auth.getClient().then(authClient => {
-    auth.getProjectId().then(project => {
-        console.log(project);
+async function authorize()
+{
+    const authFactory = new google.auth.GoogleAuth({
+        scopes: ['https://www.googleapis.com/auth/spreadsheets']
     });
+    const jwtClient = authFactory.fromJSON(
+        JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+    );
+    await jwtClient.authorize();
+    return jwtClient;
+}
+
+authorize().then(auth => {
     sheets.spreadsheets.get({
-        auth: authClient,
+        auth,
         spreadsheetId: process.env.SPREADSHEET_ID,
         ranges: ['15k Submissions!A2:Q'],
         includeGridData: true
     }, (err, range) => {
         console.log(err);
         let page = range.data.sheets[0];
-        console.log(page.data[0].rowData);
-        page.data[0].rowData.forEach(row => 
-            console.log(util.inspect(row.values[0].formattedValue) + " " + util.inspect(row.values[1].formattedValue))
-        );
+        console.log(page.data[0].rowData[0].values);
     });
 });
 
