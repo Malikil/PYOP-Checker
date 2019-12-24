312b5de77c22407ceb68ed3912f11a900c156895
Eric H <eboy.coltt5@gmail.com>
2019-12-24 15:41:32 -0800

    Tweak checkMap help message

--- commands.js
+++ commands.js
@@ -87,7 +87,7 @@ async function checkMap(msg)
     else if (args[1] == '?')
         return msg.channel.send("Usage: !check <map> [mod]\n" +
             "Map should be a link or map id\n" +
-            "(Optional) mod should be some combination of HD|HR|DT|HT|EZ. Default is nomod, unrecognised items are ignored\n" +
+            "(Optional) mod should be some combination of HD|HR|DT|HT|EZ. Default is NoMod\n" +
             "Aliases: !map");
     let mod = 0;
     if (args.length == 3)
