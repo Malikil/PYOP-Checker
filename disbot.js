c3e64ab2f02c058a84d4fe07cc408013755990d3
Eric H <eboy.coltt5@gmail.com>
2020-01-24 09:40:13 -0800

    Add update maps alias

--- disbot.js
+++ disbot.js
@@ -112,7 +112,8 @@ client.on('message', msg => {
         response = adminCommand(msg, commands.lockSubmissions);
     else if (msg.content === "!export")
         response = adminCommand(msg, commands.exportMaps);
-    else if (msg.content === "!updateMaps")
+    else if (msg.content === "!updateMaps" ||
+                msg.content === "!update")
         response = adminCommand(msg, commands.recheckMaps);
     
     if (response)
