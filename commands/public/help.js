27328c51f031de5f8b5c48c60b313e68b19fb8cc
Eric Hagen <eboy.coltt5@gmail.com>
2021-03-02 16:04:18 -0800

    Use help command to show command info
    You can now see command info by using !help [command]
    The command does use require() to get info. I'm not a huge fan of this so maybe eventually it would be nice to pass the command list from the discord client. Though this will do for now.

--- commands/public/help.js
+++ commands/public/help.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const Discord = require('discord.js');
+const validator = require('../../validator');
 
 module.exports = {
     name: "help",
     description: "Shows available commands. " +
-        "Use !help <command> for more details about the command",
+        "Use !help [command] for more details about the command",
     args: [
         { arg: 'any', name: 'command', description: "The command to get the details of" }
     ],
@@ -13,22 +14,44 @@ module.exports = {
     /**
      * @param {Discord.Message} msg 
      */
-    async run(msg) {
+    async run(msg, { command }) {
         const commandFolders = fs.readdirSync('./commands');
-        
-        const resultStr = commandFolders.map(folder => {
-            const filestr = fs.readdirSync(`./commands/${folder}`)
-                .filter(f => f.endsWith('.js'))
-                .reduce((p, c) => {
-                    if (c === "help.js")
-                        return `${p}, help`;
-                    else {
-                        const { name } = require(`../${folder}/${c}`);
-                        return `${p}, ${name}`
-                    }
-                }, '').slice(2);
-            return `\`${folder}\` - ${filestr}`;
-        }).reduce((p, c) => `${p}\n${c}`);
-        return msg.channel.send(resultStr);
+        const commands = {};
+        commandFolders.forEach(folder => {
+            const subCommands = fs.readdirSync(`./commands/${folder}`)
+            .filter(f => f.endsWith('.js'))
+            .map(file => {
+                if (file === "help.js")
+                    return this;
+                else
+                    return require(`../${folder}/${file}`);
+            });
+            commands[folder] = subCommands;
+        });
+        // If a command is given, find that command and show the description.
+        let resultStr;
+        if (command)
+            Object.keys(commands).find(folder => {
+                const comm = commands[folder].find(c =>
+                    c.name === command || (c.alias && c.alias.includes(command))
+                );
+                if (!comm)
+                    return false;
+                
+                // Instead of finding the command from the list again,
+                // just get the string here and return.
+                // Kinda jank but whatever
+                resultStr = validator.usageString(comm);
+                return true;
+            });
+        else
+            resultStr = Object.keys(commands).map(folder => {
+                const comstr = commands[folder].reduce((p, c) =>
+                        `${p}, ${c.name}`
+                    , '').slice(2);
+                return `\`${folder}\` - ${comstr}`;
+            }).reduce((p, c) => `${p}\n${c}`);
+
+        return msg.channel.send(resultStr || "No content");
     }
 }
