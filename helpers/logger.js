ff47072f397ba56ee7cc434f045784a2e53d4a82
Eric Hagen <eboy.coltt5@gmail.com>
2021-03-14 22:08:30 -0700

    Fix missing control character in logger

--- helpers/logger.js
+++ helpers/logger.js
@@ -6,7 +6,7 @@ module.exports = {
         if (typeof str !== 'string')
             str = inspect(message, false, 3, true);
         if (tag)
-            str = `\x1b[32${tag}\x1b[0m  ${str}`;
+            str = `\x1b[32m${tag}\x1b[0m  ${str}`;
         console.log(str);
     },
 
@@ -15,7 +15,7 @@ module.exports = {
         if (typeof str !== 'string')
             str = inspect(message, false, 3, true);
         if (tag)
-            str = `\x1b[32${tag}\x1b[0m  ${str}`;
+            str = `\x1b[32m${tag}\x1b[0m  ${str}`;
         console.error(str);
     }
 }
